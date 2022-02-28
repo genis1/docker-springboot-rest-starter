cad807cbf3b382394d514420d6c794ee260a3ea2
Genís Guillem Mimó <genis.guillem@linkeit.com>
2022-02-28 14:35:38 +0100

    Created test that requires clean database to ensure it is cleaned before each test.

--- src/test/java/genis/learning/docker/CustomerControllerTest.java
+++ src/test/java/genis/learning/docker/CustomerControllerTest.java
@@ -1,13 +1,16 @@
 package genis.learning.docker;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CustomerControllerTest {
 
 
@@ -24,11 +28,41 @@ public class CustomerControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
+	@Order(1)
 	void postCustomer() throws Exception {
 		mockMvc.perform(post("/customer")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{\"name\":\"foo\"}"))
 				.andExpect(status().isOk())
 				.andExpect(content().json("{\"name\":\"foo\"}"));
+	}
+
+	@Test
+	@Order(2)
+	void getCustomer() throws Exception {
+		mockMvc.perform(get("/customer?name=foo&size=10"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("{\"content\":[{\"name\":\"foo\"}]}"));
+	}
+
+	@Test
+	@Order(3)
+	void deleteCustomer() throws Exception {
+		mockMvc.perform(delete("/customer/1"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@Order(4)
+	void getEmptyCustomer() throws Exception {
+		mockMvc.perform(get("/customer/1"))
+				.andExpect(status().isUnprocessableEntity());
+	}
+
+	@Test
+	@Order(5)
+	void deleteEmptyCustomer() throws Exception {
+		mockMvc.perform(delete("/customer/1"))
+				.andExpect(status().isUnprocessableEntity());
 	}
 }
